34fcb482227db4f92d26ca73cf0f5c1bc55621f8
karlxignis <karl@karlxignis.com>
2025-08-27 23:23:06 +0200

    Creates base compoenents

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AppsComponent } from './apps/apps.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { SupportComponent } from './support/support.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent, pathMatch: 'full' },
+  { path: 'apps', component: AppsComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'support', component: SupportComponent },
+];
+
