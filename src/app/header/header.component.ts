5be9e50defe37036c3971aad7ad6d4bcc1aa8252
karlxignis <karl@karlxignis.com>
2025-08-27 23:34:11 +0200

    Starts implementing header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
 
+  isRoot(): boolean {
+    return this.router.url === '/';
+  }
 }
+
