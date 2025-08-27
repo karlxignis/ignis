80067dee5c19a045d29173427c09cb141dfd5f88
karlxignis <karl@karlxignis.com>
2025-08-28 01:21:49 +0200

    Starts implementing HomeSection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { AppsComponent } from './apps/apps.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { SupportComponent } from './support/support.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent, pathMatch: 'full' },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'apps', component: AppsComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'support', component: SupportComponent },
