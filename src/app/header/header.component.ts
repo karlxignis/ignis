6094c94fdde3cd5cb6eedcc29d6a1809857451ef
karlxignis <karl@karlxignis.com>
2025-08-28 03:33:10 +0200

    Implements navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { Component, OnDestroy } from '@angular/core';
+import { CommonModule, NgIf, NgClass } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive, NavigationEnd } from '@angular/router';
+import { Subject, filter, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, NgIf, NgClass],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  constructor(private router: Router) {}
+export class HeaderComponent implements OnDestroy {
+  isMenuOpen = false;
+  private destroy$ = new Subject<void>();
+
+  constructor(private router: Router) {
+    // Close the mobile menu after navigating
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd), takeUntil(this.destroy$))
+      .subscribe(() => (this.isMenuOpen = false));
+  }
 
   isRoot(): boolean {
     return this.router.url === '/';
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
 
